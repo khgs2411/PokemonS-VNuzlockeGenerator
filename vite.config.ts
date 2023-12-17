44f8c1443ed7e5f176acd21f3c616dd5db6620dd
Liad Goren <liad@tigloo.io>
2023-12-17 08:27:49 +0200

    Update homepage URLs in package.json and vite.config.ts
    - Updated the "homepage" URL in package.json to fix a typo.
    - Updated the "base" URL in vite.config.ts to match the corrected homepage URL.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [vue()],
-	base: "https:/khgs2411.github.io/PokemonS-VNuzlockeGenerator/",
+	base: "https://khgs2411.github.io/PokemonS-VNuzlockeGenerator/",
 	server: {
 		port: 8000,
 	},
